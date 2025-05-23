d033960934c2672e4fc1d8a4cde653f36608eb60
Yoon Sejin <pentum99@gmail.com>
2025-05-23 16:15:25 +0900

    오전필기 끝

--- 3. data_type.sql
+++ 3. data_type.sql
@@ -42,3 +42,42 @@ alter table author add column birthday date;
 alter table post add column created_time datetime;
 insert into post (id, title, author_id, created_time) values (8, 'hello', 3, '2025-05-23 14:36:30');
 alter table post add column created_time datetime default current_timestamp();
+
+
+-- 비교연산자
+select * from author where id>=2 and id<=4;
+select * from author where id between 2 and 4; --위 구문과 같은 구문
+select * from author where id in(2, 3, 4);
+select * from author where id not in(2, 3, 4);
+
+-- like : 특정 문자를 포함하는 데이터를 조회하기 위한 키워드
+select * from post where title like '%h'
+select * from post where title like '%h'
+select * from post where title like '%h%'
+
+-- regexp : 정규 표현식을 활용한 조회
+select * from post where title regexp '[a-z]'; --하나라도 알파벳 소문자가 들어있으면
+select * from post where title regexp '[가-힣]'; --하나라도 한글이 있으면
+
+
+-- 숫자 -> 날짜
+select cast(20250523 as date) (from author); -- 2025-05-23 | from을 사용해도 괜찮다.
+-- 문자 -> 날짜
+select cast('20250523' as date) (from author); -- 2025-05-23 | from을 사용해도 괜찮다.
+-- 문자 -> 숫자
+select cast('12' as signed) --signed unsigned 중에 하나 사용하면 된다다
+select cast('12' as int) --int를 쓰면 안되는 경우도 있음 예를들면 코테
+
+-- 날짜조회 방법 : 2025-05-23 14:30:25
+-- like패턴, 부등호 활용, date_format
+select * from post where created_time like '2025-05%'; --문자열처럼 조회
+select * from post where created_time >= '2025-05-01' and created_time <= '2025-05-30'; --범위를 지정하여 조회
+
+-- date format 활용
+--Y와 H는 무조건 대문자로 사용해야함
+select date_format(created_time, '%Y-%m-%d') from post;
+select date_format(created_time, '%H-%i-%s') from post;
+
+select * from post where date_format(created_time, '%Y') = '2025';--연도
+select * from post where date_format(created_time, '%m') = '05';--연도
+select * from post where date_format(created_time, '%d') = '23';--날짜
